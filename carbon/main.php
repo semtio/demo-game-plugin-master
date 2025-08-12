a5aef39de40eed21fc782bfb6ecf7738e7543f9b
semtio <semik20092@gmail.com>
2025-08-12 12:55:54 +0200

    mini fixes

--- carbon/main.php
+++ carbon/main.php
@@ -206,18 +206,19 @@ function crb_attach_plugin_options()
                 ->set_width(25),
 
 
+            Field::make('separator', 'spec_exp_author_social_network', __('Соцсети')),
             Field::make('text', 'spec_exp_author_twitter', __('Twitter (spec_exp_author_twitter)'))
-                ->set_width(10),
+                ->set_width(30),
             Field::make('text', 'spec_exp_author_linkedin', __('Linkedin (spec_exp_author_linkedin)'))
-                ->set_width(10),
+                ->set_width(30),
             Field::make('text', 'spec_exp_author_facebook', __('Facebook (spec_exp_author_facebook)'))
-                ->set_width(10),
+                ->set_width(30),
             Field::make('text', 'spec_exp_author_instagram', __('Instagram (spec_exp_author_instagram)'))
-                ->set_width(10),
+                ->set_width(30),
             Field::make('text', 'spec_exp_author_youtube', __('Youtube (spec_exp_author_youtube)'))
-                ->set_width(10),
+                ->set_width(30),
             Field::make('text', 'spec_exp_author_telegram', __('Telegram (spec_exp_author_telegram)'))
-                ->set_width(10),
+                ->set_width(30),
         ]);
 }
 
